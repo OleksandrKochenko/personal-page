fdd44ccdc8acee39f745d2043df14d29f2d1ebfe
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-02-02 22:22:31 +0200

    + comments API project

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -3,6 +3,7 @@ export const projects = [
     id: 'a6749e4f8fd8',
     title: 'Tetris Game',
     type: 'PET',
+    scope: 'FRONT',
     description:
       'Simple implementation of wellknown puzzle game. There are 7 levels with different falling down speed, pause and start buttons, current and total score.',
     appLink: 'https://oleksandrkochenko.github.io/tetris/',
@@ -27,6 +28,7 @@ export const projects = [
     id: '982cf4bb34e9',
     title: 'News App',
     type: 'PET',
+    scope: 'FRONT',
     description:
       'News App (The New York Times API). Search by query, topic and date. Responsive design, light/dark mode. Favorite and Read pages. Embedded weather window.',
     appLink: 'https://maksym-babich.github.io/News-app/',
@@ -55,9 +57,10 @@ export const projects = [
     id: 'f85e1f78d33d',
     title: 'Image Finder',
     type: 'PET',
+    scope: 'FRONT',
     description:
       'Finds images by query. Full size image view, navigation through images, load more images. Responsive design',
-    appLink: 'https://oleksandrkochenko.github.io/goit-react-hw-04-images//',
+    appLink: 'https://oleksandrkochenko.github.io/goit-react-hw-04-images/',
     codeLink: 'https://github.com/OleksandrKochenko/goit-react-hw-04-images',
     img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706565311/PersonalPage/images/ImageFinder2_ylkrrl.jpg',
     techStack: [
@@ -76,6 +79,35 @@ export const projects = [
       {
         name: 'React',
         img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706562092/PersonalPage/icons/react-svgrepo-com_lotxms.svg',
+      },
+    ],
+  },
+  {
+    id: 'f85e1f78d33d',
+    title: 'Comments API',
+    type: 'PET',
+    scope: 'BACK',
+    description:
+      'REST API for Comments application, that provides CRUD operations for user comments and user authorization',
+    appLink: 'https://api-comments-chi.vercel.app/',
+    codeLink: 'https://github.com/OleksandrKochenko/dZENcode-test-assignment',
+    img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706903591/PersonalPage/images/Comments_API_duzkii.jpg',
+    techStack: [
+      {
+        name: 'NodeJs',
+        img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706904724/PersonalPage/icons/node-16-svgrepo-com_la8ibe.svg',
+      },
+      {
+        name: 'MongoDB',
+        img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706904723/PersonalPage/icons/mongodb-svgrepo-com_dzfyv9.svg',
+      },
+      {
+        name: 'Docker',
+        img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706904723/PersonalPage/icons/docker-svgrepo-com_xnjoum.svg',
+      },
+      {
+        name: 'Vercel',
+        img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706904723/PersonalPage/icons/vercel-logo-svgrepo-com_xobmio.svg',
       },
     ],
   },
