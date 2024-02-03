d78d8520d892a9bc20fa415fc94efbd310c1d27b
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-02-03 22:55:02 +0200

    fill sertificates, media query for gallery

--- src/data/certificates-data.js
+++ src/data/certificates-data.js
@@ -21,9 +21,36 @@ export const certificates = [
     id: 'eb88379d940c',
     title: 'IELTS Certificate',
     preview:
-      'https://res.cloudinary.com/dvloxectq/image/upload/v1706980964/Certificates/thumb/IELTS_certificate_sftxuh.jpg',
-    img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706977231/Certificates/IELTS_certificate_uiwdsl.jpg',
+      'https://res.cloudinary.com/dvloxectq/image/upload/v1706992461/Certificates/thumb/IELTS_certificate_il2clh.jpg',
+    img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706992414/Certificates/IELTS_certificate_hdw2na.jpg',
     height: 3469,
     width: 2474,
+  },
+  {
+    id: '61ad1d8898ef',
+    title: 'EPAM Certificate',
+    preview:
+      'https://res.cloudinary.com/dvloxectq/image/upload/v1706980964/Certificates/thumb/IT_Fundamentals_for_Ukrainian_Switchers_2022-1_xjcjwf.jpg',
+    img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706978871/Certificates/IT_Fundamentals_for_Ukrainian_Switchers_2022-1_w5bc8d.jpg',
+    height: 1239,
+    width: 1753,
+  },
+  {
+    id: '84cc72cf2679',
+    title: 'Polish A1',
+    preview:
+      'https://res.cloudinary.com/dvloxectq/image/upload/v1706980966/Certificates/thumb/Polish_A1_z7z5wm.jpg',
+    img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706977480/Certificates/Polish_A1_kfmqfj.jpg',
+    height: 3504,
+    width: 2480,
+  },
+  {
+    id: '8f385bb0967c',
+    title: 'Polish A2',
+    preview:
+      'https://res.cloudinary.com/dvloxectq/image/upload/v1706980967/Certificates/thumb/Polish_A2_ar3mgs.jpg',
+    img: 'https://res.cloudinary.com/dvloxectq/image/upload/v1706977480/Certificates/Polish_A2_n8y6jx.jpg',
+    height: 3504,
+    width: 2480,
   },
 ];
